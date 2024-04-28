839451394f723d2663f7d6baf42500fc7aced7d8
Ömer Kahveci <omerkahveci53@hotmail.com>
2024-04-28 13:28:57 +0300

    initial commit

---
+++ services/user-service.tsx
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const ApiUrl = process.env.NEXT_PUBLIC_API_URL;
+const PageSize = Number(process.env.NEXT_PUBLIC_PAGE_SIZE);
+const UserService = {
+
+    GetAll: async (limit=PageSize, skip=0) =>{
+        const url = `${ApiUrl}/users/?limit=${limit}&skip=${skip}&select=firstName,lastName,phone,email,company,domain,image`;
+        const response = await axios.get(url)
+
+        return response;
+
+    }
+
+
+}
+export default UserService;
